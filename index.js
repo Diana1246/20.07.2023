71d12625a25f6a5aaf58fe189ad56eb5da9e69e2
Diana1246 <109847803+Diana1246@users.noreply.github.com>
2023-07-20 21:27:01 +0300

    20.07.2023

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ import foodTpl from '../templates/food.handlebars';
 
  const markup = foodTpl(food);
  foodList.innerHTML = markup;
+
+
+ const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+  
+  const themeSwitchToggle = document.getElementById('theme-switch-toggle');
+  
+ 
+  const savedTheme = localStorage.getItem('theme');
+  
+ 
+  if (savedTheme === Theme.DARK) {
+    document.body.classList.add(Theme.DARK);
+    themeSwitchToggle.checked = true;
+  } else {
+    document.body.classList.add(Theme.LIGHT);
+  }
+ 
+  function handleThemeToggle() {
+    if (themeSwitchToggle.checked) {
+      document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+      localStorage.setItem('theme', Theme.DARK);
+    } else {
+      document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+      localStorage.setItem('theme', Theme.LIGHT);
+    }
+  }
+  
+ 
+  themeSwitchToggle.addEventListener('change', handleThemeToggle);
+  
