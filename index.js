2d609ce408c1690c5c31cf7925da693425ec4d64
Diana1246 <109847803+Diana1246@users.noreply.github.com>
2023-07-21 21:26:17 +0300

    20.07.2023

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import food from './menu.json';
-import foodTpl from '../templates/food.handlebars';
+//import food from './menu.json';
+//import foodTpl from '../templates/food.handlebars';
 
- const foodList = document.querySelector('.js-menu');
+ //const foodList = document.querySelector('.js-menu');
 
- const markup = foodTpl(food);
- foodList.innerHTML = markup;
+ //const markup = foodTpl(food);
+ //foodList.innerHTML = markup;
 
 
  const Theme = {
